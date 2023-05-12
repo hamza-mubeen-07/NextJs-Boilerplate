0b31ef011827393506715d323aaa384ff8367609
Hamza Mubeen <hamza@glowfishlabs.com>
2023-05-12 17:41:39 +0500

    ci(PET-5): added login page.

---
+++ src/app/[lang]/login/page.tsx
@@ -0,0 +1,22 @@
+import { store } from '@/store';
+import Provider from '@/store/Provider';
+import LoginForm from '@/component/loginForm';
+import { getLoginPageContent } from '@/store/content/content-thunk';
+import { iAllowedLocale } from '@/constants/commonConstant';
+import { updateLang } from '@/store/content/content-slice';
+
+const Page = async ({ params }: { params: { lang: iAllowedLocale } }) => {
+  await store.dispatch(getLoginPageContent(params.lang));
+  store.dispatch(updateLang(params.lang));
+  const content = store.getState().content;
+  return (
+    <div>
+      <Provider>
+        <h1>{JSON.stringify(params)}</h1>
+        <LoginForm loginPageContent={content} />
+      </Provider>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,19 +1,10 @@
-import { store } from '@/store';
 import Provider from '@/store/Provider';
-import LoginForm from '@/component/loginForm';
-import { getLoginPageContent } from '@/store/content/content-thunk';
-import { iAllowedLocale } from '@/constants/commonConstant';
-import { updateLang } from '@/store/content/content-slice';
 
-const PetListing = async ({ params }: { params: { lang: iAllowedLocale } }) => {
-  await store.dispatch(getLoginPageContent(params.lang));
-  store.dispatch(updateLang(params.lang));
-  const content = store.getState().content;
+const PetListing = async () => {
   return (
     <div>
       <Provider>
-        <h1>{JSON.stringify(params)}</h1>
-        <LoginForm loginPageContent={content} />
+        <h1> Home </h1>
       </Provider>
     </div>
   );
